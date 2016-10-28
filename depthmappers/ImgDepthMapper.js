9ec8a13c58995045330da982d93b5c98b3685a65
Juha Tauriainen <juha@bin.fi>
2016-10-28 18:49:38 +0300

    Changed grunt and jshint to gulp and eslint

--- depthmappers/ImgDepthMapper.js
+++ depthmappers/ImgDepthMapper.js
@@ -2,16 +2,14 @@
 // Parses an <img>'s pixel data and generates a depth map
 //
 MagicEye.ImgDepthMapper = MagicEye.DepthMapper.extend({
-
   constructor: function (img) {
     this.img = img;
   },
 
   make: function () {
-
     // not async, assumes img is already loaded
-    var canvas = document.createElement('canvas'),
-        context = canvas.getContext("2d");
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
     canvas.width = this.img.width;
     canvas.height = this.img.height;
     context.drawImage(this.img, 0, 0, canvas.width, canvas.height);
@@ -19,12 +17,15 @@ MagicEye.ImgDepthMapper = MagicEye.DepthMapper.extend({
   },
 
   depthMapFromCanvas: function (canvas) {
-    var x, y, offset,
-        context = canvas.getContext("2d"),
-        depthMap = [],
-        width = canvas.width,
-        height = canvas.height,
-        pixelData = context.getImageData(0, 0, width, height).data;
+    var x;
+    var y;
+    var offset;
+    var context = canvas.getContext('2d');
+    var depthMap = [];
+    var width = canvas.width;
+    var height = canvas.height;
+    var pixelData = context.getImageData(0, 0, width, height).data;
+
     for (y = 0; y < height; y++) {
       depthMap[y] = new Float32Array(width);
       offset = width * y * 4;
